153ea4bbfd31199e46e859685d852e136323fce3
Zipeth <markchristensen1010@gmail.com>
2023-07-27 16:49:39 +0100

    to-do's working

--- src/Components/TaskForm.tsx
+++ src/Components/TaskForm.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const TaskForm = () => {
   const [taskBody, setTaskBody] = useState<string>("");
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [completedTasks, setCompletedTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<{ task: string; completed: boolean }[]>(
+    []
+  );
+
+  useEffect(() => {
+    setTasks(tasks);
+  }, []);
+
+  const handleDelete = (task: { task: string; completed: boolean }) => {
+    const index = tasks.indexOf(task);
+    if (index > -1) {
+      tasks.splice(index, 1);
+    }
+    let newTasks = [...tasks];
+    setTasks(newTasks);
+  };
 
   const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (tasks.length === 0) {
-      setTasks([taskBody]);
+      setTasks([
+        {
+          task: taskBody,
+          completed: false,
+        },
+      ]);
       setTaskBody("");
     } else {
-      setTasks([...tasks, taskBody]);
+      setTasks([...tasks, { task: taskBody, completed: false }]);
       setTaskBody("");
     }
-    console.log(tasks);
   };
 
   return (
@@ -33,17 +51,37 @@ const TaskForm = () => {
         <button type="submit">Add Task!</button>
       </form>
       <h3>{tasks.length} Tasks to do!</h3>
-      <ul>
-        {tasks.map((task) => {
-          return (
-            <li>
-              {task}
-              <input type="checkbox"></input>
-            </li>
-          );
-        })}
-      </ul>
-      <h3>Completed tasks!</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Completed?</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task, i) => {
+            return (
+              <tr>
+                <td key={i}>{task.task}</td>
+                <td>
+                  <input type="checkbox" id="myCheck"></input>
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      handleDelete(task);
+                      console.log(task);
+                    }}
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 };
